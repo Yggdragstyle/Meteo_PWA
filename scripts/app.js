beef6fe7f8420b268f16529c97e82153fde89b07
Ygg <dans.la.com@gmail.com>
2018-01-15 16:31:04 +0100

    remove Service Worker from script

--- scripts/app.js
+++ scripts/app.js
@@ -319,7 +319,7 @@ console.log("APP JS");
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
            // .register('./service-worker.js')
-           .register('scripts/service-worker.js')
+           .register('./service-worker.js')
            .then(function() { console.log('Service Worker Registered'); });
 }
 
