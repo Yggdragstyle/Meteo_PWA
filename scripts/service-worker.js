6c168c6399fb215df44bad42e01f746f60e9f55c
Ygg <dans.la.com@gmail.com>
2018-01-15 16:23:49 +0100

    add dote for Service Worker url

---
+++ scripts/service-worker.js
@@ -0,0 +1,37 @@
+var cacheName = 'meteo_PWA-app-v-1.1';
+
+var filesToCache = [
+	'./',
+  	'./index.html',
+	'./scripts/app.js',
+	'./styles/inline.css',
+	'./images/clear.png',
+	'./images/ic_refresh_white_24px.svg'
+];
+
+// Create the cache files
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+// EDIT the cache files
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
